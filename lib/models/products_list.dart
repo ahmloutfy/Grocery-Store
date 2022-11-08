33235850a33222b9098f2e8d13b6da7e4f8be931
ahmloutfy <47849967+ahmloutfy@users.noreply.github.com>
2022-11-08 21:42:51 +0200

    ASGS 8/11/2022 (0)

--- lib/models/products_list.dart
+++ lib/models/products_list.dart
@@ -1,1 +1,25 @@
+class Fruits {
+  final String? image;
+  final String? fruitName;
+  final String? fruitCategory;
+  final String? fruitPrice;
+  final bool? addedToCart;
+
+  Fruits(
+      {this.image,
+      this.fruitName,
+      this.fruitCategory,
+      this.fruitPrice,
+      this.addedToCart});
+}
+
+// final Fruits banana = Fruits(
+//     image: ,
+//     fruitName: ,
+//     fruitCategory: ,
+//     fruitPrice: ,
+//     addedToCart: ,
+// );
+
+List fruits = [];
 
